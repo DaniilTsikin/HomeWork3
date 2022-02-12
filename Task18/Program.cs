1bf2b4b228489a350c0f3bd6f0ae556c026989de
Daniil <dtsikin@mail.ru>
2022-02-12 18:56:49 +0500

    Решена задача 18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool Truth(bool arg1, bool arg2)
+{
+    bool sum = !(arg1 ^ arg2);
+    Console.WriteLine(sum);
+    bool prct = !arg1 & arg2;
+    Console.WriteLine(prct);
+    bool result = false;
+    if(sum == prct)
+    {
+        result = true;
+    }
+    return result;
+}
+bool X = true;
+bool Y = false;
+bool res = Truth(X, Y);
+Console.WriteLine(res);
