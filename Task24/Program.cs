71c02eab203e71363e8e3da6b43d5467bb9077f8
Daniil <dtsikin@mail.ru>
2022-02-13 17:11:05 +0500

    Изменили оформление Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,5 +1,5 @@
 ﻿//  Найти кубы чисел от 1 до N
-Console.Write("Введите любое число от 1 и больше ");
+Console.Write("Введите любое число от 1 и больше: ");
 int input = int.Parse(Console.ReadLine());
 
 for (int n = 1; n <= input; n++)
