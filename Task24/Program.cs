84b41d9950090d89b9caa9a5f4f72687c19521cf
Daniil <dtsikin@mail.ru>
2022-02-13 14:45:39 +0500

    Решили задачу 24

---
+++ Task24/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Найти кубы чисел от 1 до N
+Console.Write("Введите любое число от 1 и больше ");
+int input = int.Parse(Console.ReadLine());
+
+for (int n = 1; n <= input; n++)
+{
+    Console.Write($"{n*n*n} ");
+}
+
